4d390fcbafdfff74f067f5ef198618476b26e711
eudus <eudusb@shaw.ca>
2026-03-10 16:02:06 -0700

    Added base files

---
+++ src/main/java/CPSC482Team/DSM/App.java
@@ -0,0 +1,13 @@
+package CPSC482Team.DSM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
